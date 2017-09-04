635a4c320157dec1feb1dc55da9e362215ca3536
Paul Meems <p.meems@akkerweb.nl>
2017-09-04 16:35:40 +0200

    Make the  stallions listing page work

--- src/app/stallions/list/stallions-listings.component.ts
+++ src/app/stallions/list/stallions-listings.component.ts
@@ -1,13 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import {Stallion} from "../../shared/stallion.model";
+import {FirebaseService} from "../../shared/firebase.service";
 
 @Component({
-  selector: 'app-stallions-listings',
-  templateUrl: './stallions-listings.component.html',
-  styleUrls: ['./stallions-listings.component.scss']
+  selector: "app-stallions-listings",
+  templateUrl: "./stallions-listings.component.html",
+  styleUrls: ["./stallions-listings.component.scss"]
 })
 export class StallionsListingsComponent implements OnInit {
+  private errorMsg = null;
+  private stallions: Stallion[];
+  private loading = true;
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) {
+    this.loading = true;
+    this.firebaseService.loadStallions()
+      .subscribe(data => {
+          this.stallions = data;
+          // Sort by name:
+          this.stallions.sort((left, right): number => {
+            if (left.name < right.name) {
+              return 1;
+            }
+            if (left.name > right.name) {
+              return -1;
+            }
+            return 0;
+          });
+
+          this.loading = false;
+        },
+        error => {
+          this.loading = false;
+          this.errorMsg = error;
+          console
+            .log(error);
+        },
+        () => {
+          // TODO: Never gets called:
+          this.loading = false;
+          console.log("In complete");
+        }
+      );
+  }
 
   ngOnInit() {
   }
